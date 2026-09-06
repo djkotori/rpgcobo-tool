ede145fa6dd9c5b7cebd3823a2cf9272629e3a11
Yutaka Owada <djkotori@gmail.com>
2026-09-06 12:37:30 +0900

    - You can now see message preview in the event command "Message" or "Preset UI" - Improved the enemy group placement UI. - Fixed an issue where members could not be removed using the event command "Member Change". - Fixed a bug where events deleted using the event command "Vanish Event" would be restored after loading. - Fixed an issue where, when defeated in a battle from an event command, only the "Defeat Macro(X002)" was executed. - You can now specify the marker ⇒ point when specifying the destination from the portal or portal door. Useful for specifying the destination of a one-way portal. - Fixed an issue where an error would occur when specifying "Joined to Party" as an event condition. - Fixed an issue where the item type specification was not reflected correctly in the item selection dialog. - You can now specify variables and place map assets using the event command ``Place Map Asset.'' - Other bug fixes and minor improvements have been made.

--- tools/magicavoxel_open.ps1
+++ tools/magicavoxel_open.ps1
@@ -19,10 +19,18 @@ $INNER_DIR = "\MagicaVoxel-0.99.7.2-win64"
 GetTool -TOOL_DIR $TOOL_DIR -TOOL_URL $TOOL_URL -INNER_DIR $INNER_DIR
 
 # junction
-if( Test-Path ..\work\MagicaVoxel\vox\3x3x3.vox) {
-    Remove-Item -Path ..\work\MagicaVoxel\vox -Recurse -Force
-    &cmd /c mklink /j ..\work\MagicaVoxel\vox ..\project\resource\vox
+$VOX_DIR = [System.IO.Path]::GetFullPath((Join-Path $PSScriptRoot "..\work\MagicaVoxel\vox"))
+$VOX_TARGET_DIR = [System.IO.Path]::GetFullPath((Join-Path $PSScriptRoot "..\project\resource\vox"))
+$voxItem = Get-Item -LiteralPath $VOX_DIR -Force -ErrorAction SilentlyContinue
+
+if ($null -ne $voxItem) {
+    if (($voxItem.Attributes -band [System.IO.FileAttributes]::ReparsePoint) -ne 0) {
+        [System.IO.Directory]::Delete($VOX_DIR, $false)
+    } else {
+        Remove-Item -LiteralPath $VOX_DIR -Recurse -Force
+    }
 }
+New-Item -ItemType Junction -Path $VOX_DIR -Target $VOX_TARGET_DIR | Out-Null
 
 Write-Output "SK-OUTPUT popen_launch"
 
